5a68a044b30210262a8c0dc4ea558ca618bf72dc
Pooja Kulkarni <pk@Poojas-MBP.fritz.box>
2020-01-21 00:12:15 +0100

    flask_injector to inject a dependency - data service
    Signed-off-by: Pooja Kulkarni <pk@Poojas-MBP.fritz.box>

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from connexion.resolver import RestyResolver
 import connexion
+from injector import Binder
+from flask_injector import FlaskInjector
+from services.provider import ItemsProvider
+
+
+def configure(binder: Binder) -> Binder:
+    binder.bind(
+        ItemsProvider,
+        ItemsProvider([{"Name": "Test 1"}])
+    )
 
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='swagger/')
     app.add_api('my_first_app.yaml', resolver=RestyResolver('api'))
+    FlaskInjector(app=app.app, modules=[configure])
     app.run(port=9090)
